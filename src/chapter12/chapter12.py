2799fa921f5c42682a0fbbd7101b916136092c6c
Mike Han <mmhan2u@gmail.com>
2011-09-20 16:38:24 +0800

    + Using BoxSizer()

--- src/chapter12/chapter12.py
+++ src/chapter12/chapter12.py
@@ -4,19 +4,28 @@ import wx
 
 app = wx.App()
 
-win = wx.Frame(None, title = "Simple Editor")
-win.Show()
+win = wx.Frame(None, title = "Simple Editor", size = (410, 335))
+bkg = wx.Panel(win)
 
-loadButton = wx.Button(win, label = 'Open',
-                       pos=(225,5), size=(80, 25))
-saveButton = wx.Button(win, label = 'Save',
-                       pos=(315,5), size=(80,25))
+loadButton = wx.Button(bkg, label = 'Open')
+saveButton = wx.Button(bkg, label = 'Save')
 
-filename = wx.TextCtrl(win, pos=(5,5), size = (210,25))
+filename = wx.TextCtrl(bkg)
 
-contents = wx.TextCtrl(win, pos = (5, 35), size = (390,260),
+contents = wx.TextCtrl(bkg,
                        style = wx.TE_MULTILINE | wx.HSCROLL)
 
+hbox = wx.BoxSizer()
+hbox.Add(filename, proportion = 1, flag = wx.EXPAND)
+hbox.Add(loadButton, proportion=0, flag = wx.LEFT, border = 5)
+hbox.Add(saveButton, proportion=0, flag = wx.LEFT, border = 5)
 
+vbox = wx.BoxSizer(wx.VERTICAL)
+vbox.Add(hbox, proportion = 0, flag = wx.EXPAND | wx.ALL, border = 5)
+vbox.Add(contents, proportion = 1,
+         flag = wx.EXPAND | wx.LEFT | wx.LEFT | wx.LEFT, border = 5)
 
-win = app.MainLoop()
+bkg.SetSizer(vbox)
+win.Show()
+
+app.MainLoop()
