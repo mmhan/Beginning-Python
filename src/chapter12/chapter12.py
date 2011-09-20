1547c9ddf70208c78adc50c2e70a65561ac1283d
Mike Han <mmhan2u@gmail.com>
2011-09-20 16:50:51 +0800

    + Event handling

--- src/chapter12/chapter12.py
+++ src/chapter12/chapter12.py
@@ -2,13 +2,32 @@
 
 import wx
 
-app = wx.App()
+def load(event):
+    '''
+    This function will receive and load a text file from given src and show on the screen
+    '''
+    file = open(filename.GetValue())
+    contents.SetValue(file.read())
+    file.close()
 
+def save(event):
+    '''
+    This function will use the content of the large text box and save it in given file.
+    '''
+    file = open(filename.GetValue(), 'w')
+    file.write(contents.GetValue())
+    file.close()
+
+app = wx.App()
 win = wx.Frame(None, title = "Simple Editor", size = (410, 335))
+
 bkg = wx.Panel(win)
 
 loadButton = wx.Button(bkg, label = 'Open')
+loadButton.Bind(wx.EVT_BUTTON, load)
+
 saveButton = wx.Button(bkg, label = 'Save')
+saveButton.Bind(wx.EVT_BUTTON, save)
 
 filename = wx.TextCtrl(bkg)
 
