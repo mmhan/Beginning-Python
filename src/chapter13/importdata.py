ad132467d54752a47f9412a7747df6cd5f24d5d3
Mike Han <mmhan2u@gmail.com>
2011-09-21 15:57:32 +0800

    + Chapter 13 - Database support

---
+++ src/chapter13/importdata.py
@@ -0,0 +1,40 @@
+import sqlite3 as sqlite
+
+def convert(value):
+    if value.startswith('~'):
+        return value.strip('~')
+    if not value:
+        value = '0'
+    return float(value)
+
+def createDbFromFile():
+    conn = sqlite.connect('food.db')
+    curs = conn.cursor()
+    
+    curs.execute('''
+    CREATE TABLE food(
+        id        TEXT    PRIMARY KEY,
+        desc      TEXT,
+        water     FLOAT,
+        kcal      FLOAT,
+        protein   FLOAT,
+        fat       FLOAT,
+        ash       FLOAT,
+        carbs     FLOAT,
+        fiber     FLOAT,
+        sugar     FLOAT
+    )
+    ''')
+    field_count=10
+    
+    query = 'INSERT INTO food VALUES (?,?,?,?,?,?,?,?,?,?)'
+    
+    for line in open('ABBREV.txt'):
+        fields = line.split('^')
+        vals = [convert(f) for f in fields[:field_count]]
+        curs.execute(query, vals)
+        
+    conn.commit()
+    conn.close()
+    
+createDbFromFile()
